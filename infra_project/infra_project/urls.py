3e83b9f3873973a3b3709df4e8d49884ee3ff725
ruslan.nusalov@gmail.com <rnusalov@gmail.com>
2022-08-06 12:27:48 +0300

    add test with flake8 and tests07

--- infra_project/infra_project/urls.py
+++ infra_project/infra_project/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('infra_app.urls', namespace='infra_app')),
+    path('/', include('infra_app.urls', namespace='infra_app')),
 ]
